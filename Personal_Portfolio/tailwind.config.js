9ee6560d59beac6c7b183ed2168021e1e0da38e9
Neo <lordneobarnachea@gmail.com>
2024-01-04 15:34:52 +0800

    added tailwind colors

--- Personal_Portfolio/tailwind.config.js
+++ Personal_Portfolio/tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          "black": '#000000',
+          "neutral": '#404040',
+          "white": '#FFFFFF',
+        },
+        zinc: {
+          100: '#F4F4F5',
+          200: '#E4E4E7',
+          300: '#D4D4D8',
+          500: '#71717A',
+          800: '#27272A',
+        },
+        error:{
+          500: 'ef4444'
+        }
+      },
+    },
   },
   plugins: [],
 }
