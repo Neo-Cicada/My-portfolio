34f882646f34e00b46d91cde0f110078f39d0dcc
Neo <lordneobarnachea@gmail.com>
2024-01-06 15:51:12 +0800

    added tailwind config

--- Personal_Portfolio/src/components/pages/Intro.jsx
+++ Personal_Portfolio/src/components/pages/Intro.jsx
@@ -9,9 +9,9 @@ export default function () {
             <div className='flex justify-center  md:w-1/2 '>
                 <img src={LaptopOppa} alt="Oppa with laptop" className='h-[100%]' />
             </div>
-            <div className=' px-4 my-8 md:m-0 flex flex-col items-start md:w-1/2 lg:justify-end md:justify-center md:gap-14'>
-                <p>Hello I'm Lord Neo Barnachea.</p>
-                <p>Fullstack Developer</p>
+            <div className=' px-4 my-8 md:m-0 flex flex-col items-start md:w-1/2 lg:justify-end md:justify-center md:gap-6'>
+                <p className='text-display leading-display tracking-display'>Hello I'm <span className='font-extrabold'>Lord Neo Barnachea.</span></p>
+                <p className='text-display leading-display tracking-display font-extrabold'> Fullstack <span className='text-outline'>Developer</span></p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis cumque, nobis voluptate sed quia, cum suscipit accusamus ratione eligendi corporis aperiam enim illum qui cupiditate vel dignissimos quisquam, obcaecati sint!
                     xpedita, reiciendis quidem iusto solutatur et autem? Suscipit quisquam ipsam impedit nemo!
                 </p>
--- Personal_Portfolio/tailwind.config.js
+++ Personal_Portfolio/tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'sora': ['Sora', 'sans-serif'],
+      },
       colors: {
         primary: {
           "black": '#000000',
@@ -20,10 +23,85 @@ export default {
           800: '#27272A',
         },
         error:{
-          500: 'ef4444'
+          500: '#ef4444',
         }
+      },
+      fontSize: {
+        'display': '48px',
+        'display-medium': '24px', 
+        'dispay-subhead': '40px',
+        'h1':'36px',
+        'h2':'32px',
+        'h3':'28px',
+        'h4':'24px',
+        'h5':'20px',
+        'h6':'16',
+        'paragraph-1': '20px', 
+        'paragraph-2': '16px', 
+        'paragraph-3': '12px', 
+        'button':'20px',
+        'button-2':'16px',
+        'underline': '20px',
+        
+      },
+      lineHeight: {
+        'display': '56px',
+        'display-medium': '28px',
+        'dispay-subhead': '48px',
+        'h1':'44px',
+        'h2':'40px',
+        'h3':'32px',
+        'h4':'28px',
+        'h5':'24px',
+        'h6':'20px',
+        'paragraph-1': '24px', 
+        'paragraph-2': '20px', 
+        'paragraph-3': '16px', 
+        'button':'24px',
+        'button-2':'20px',
+
+        'underline': '24px',
+       
+      },
+      letterSpacing: {
+        'display': '-2%',
+        'display-medium': '-2%',
+        'display-subhead': '-2%',
+        'h1':'-2%',
+        'h2':'-2%',
+        'h3':'-2%',
+        'h4':'-2%',
+        'h5':'-2%',
+        'h6':'-2%',
+        'paragraph-1': '2%', 
+        'paragraph-2': '2%', 
+        'paragraph-3': '2%', 
+        'button':'2%',
+        'button-2':'2%',
+
+        'underline': '24px',
+      },
+      borderWidth: {
+        '3': '3px',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-outline': {
+          WebkitTextFillColor: 'transparent',
+          WebkitTextStroke: 'currentColor',
+          WebkitTextStrokeWidth: '3px',
+        },
+        '.text-outline-medium': {
+          WebkitTextFillColor: 'transparent',
+          WebkitTextStroke: 'currentColor',
+          WebkitTextStrokeWidth: '2px', // Adjust the stroke width as needed
+        },
+      };
+
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
 }
