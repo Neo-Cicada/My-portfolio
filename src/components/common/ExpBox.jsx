67ce2358d5dd9956db57a4fc23df7345175b26f3
Neo <lordneobarnachea@gmail.com>
2024-01-13 21:12:46 +0800

    fix the experience box

--- src/components/common/ExpBox.jsx
+++ src/components/common/ExpBox.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import BoomTechIcon from '../../assets/boomtech-icon.jpeg'
 export default function () {
     return (
-        <div className='border border-white w-[93%] rounded-lg h-40'>
+        <div className='border border-white w-[93%] rounded-lg flex flex-col justify-between h-40 px-9 py-9'>
 
+            <div className='flex justify-between items-center'>
+                <div className='flex justify-center items-center'>
+                    <img src={BoomTechIcon} alt="boom tech icon" height='32px' width='32px' className='rounded-full mr-4' />
+                    <p className='text-white'> Web Developer Intern at Boom Technologies </p>
+                </div>
+                <p className='text-white'>Dec 2023 - Present</p></div>
+            <div>
+                <p className='text-zinc-300'>Lorem ipsum dolor sit,laborum adipisci!</p>
+            </div>
         </div>
     )
 }
--- src/components/pages/Experience.jsx
+++ src/components/pages/Experience.jsx
@@ -3,10 +3,9 @@ import ExpBox from '../common/ExpBox'
 
 export default function () {
     return (
-        <div className='py-5 bg-primary-black md:h-[80vh] lg:h-[100vh] max-w-screen-2xl mx-auto min-w-[320px] max-h-[800px] min-h-[400px]'>
+        <div className='py-5 bg-primary-black max-w-screen-2xl mx-auto min-w-[320px] max-h-[800px] min-h-[400px]'>
             <p className='text-display text-primary-white text-center '>My <span className='font-bold'>Experience</span></p>
             <div className='flex flex-col justify-center items-center gap-5'>
-                <ExpBox/>
                 <ExpBox/>
             </div>
         </div>
