b8075b632c7532c154b9fc516314255df9a97b91
Neo <lordneobarnachea@gmail.com>
2024-01-14 22:18:56 +0800

    testing

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -13,7 +13,7 @@ const AboutMe = () => {
                     <p className='text-paragraph-2 leading-paragraph-1 tracking-paragraph-1 md:text-paragraph-1 text-zinc-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quis molestiae id repellendus? Asperiores quis delectus totam voluptatem cumque dicta, eligendi consequuntur reiciendis omnis doloribus recusandae perferendis debitis quaerat adipisci!</p>
                     <p className='text-paragraph-2 leading-paragraph-1 tracking-paragraph-1 md:text-paragraph-1 text-zinc-500'>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, facilis, ipsa quas quibusdam quasi architecto sequi nisi veniam et maxime cum ex consequatur sunt ratione blanditiis excepturi dolores, tempore repellendus.
-                        obcaecati reiciendis vel! Sequi veniam consectetur voluptate doloremque molestiae dolor, facilis inventore?
+                        reiciendis vel! Sequi veniam consectetur voluptate doloremque molestiae dolor, facilis inventore?
                     </p>
                 </div>
             </div>
