254ebc41418689ef1661212e08c786422c8af44c
Neo <lordneobarnachea@gmail.com>
2024-01-21 20:53:29 +0800

    add bms project

--- src/components/common/ProjectBox.jsx
+++ src/components/common/ProjectBox.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import website from '../../assets/under-construction.png'
-import open from '../../assets/icons/openlink.svg' 
-export default function ({projectImg, name ="Websites are under constructions", number, description}) {
+import open from '../../assets/icons/openlink.svg'
+export default function ({ projectImg = website,
+    gitlink,
+    name = "Websites are under constructions",
+    number,
+    description = "UNDER CONSTRUCTION" }) {
     return (
         <div className=' w-[95%] flex flex-col md:flex-row'>
             <div className='flex justify-center md:h-80 lg:h-96 md:w-1/2 md:justify-center md:items-center'>
-                <div className='bg-white w-[90%] h-[367px] md:w-[80%]md:h-[90%] rounded-[18.76px] overflow-hidden'>
-                    <img src={website}  alt="under construction" />
+                <div className='bg-white w-[90%] max-h-[367px] md:w-[80%]md:h-[90%] rounded-[18.76px] overflow-hidden'>
+                    <img src={projectImg} alt="under construction" />
                 </div>
             </div>
             <div className='flex justify-center md:h-80 lg:h-96 md:w-1/2 md:justify-center md:items-center'>
                 <div className='w-[90%] h-[367px] md:w-[90%] md:h-[90%] flex flex-col justify-around'>
                     <p className='font-extrabold text-white text-dispay-subhead md:text-display'>{number}</p>
                     <p className='font-bold text-white text-h3 md:text-h2'>{name}</p>
-                    <p className='md:text-paragraph-1 text-slate-400'>UNDER CONSTRUCTION</p>
-                    <img src={open} width={'20px'} height={'20px'} alt="" />
+                    <p className='md:text-paragraph-1 text-slate-400'>{description}</p>
+                    <a href={gitlink} target='_'><img src={open} width={'20px'} height={'20px'} alt="" /></a>
                 </div>
             </div>
         </div>
--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import ProjectBox from '../common/ProjectBox'
 import { Element } from 'react-scroll'
+import bms from '../../assets/bms.png'
 const Project = () => {
     return (
         <Element name="project">
             <div className='flex flex-col items-center bg-primary-black max-w-screen-2xl mx-auto min-w-[320px] gap-y-14 min-h-[400px]'>
-                <p className='text-display leading-display tracking-display py-4 text-white text-center'>My <span className='font-bold'>Projects</span></p>
-                <ProjectBox number="1" name='Rapid Room'/>
-                <ProjectBox number="2" name='Bookish Boulevard'/>
-                <ProjectBox number="3" name='Barangay Management System'/>
+                <p className='py-4 text-center text-white text-display leading-display tracking-display'>My <span className='font-bold'>Projects</span></p>
+                <ProjectBox
+                    number="1"
+                    name='Barangay Management System'
+                    description="Led the design and implementation of a comprehensive Barangay Management System, employing React, Material-UI, Firebase, and Node.js technologies to facilitate efficient communication and request management between administrators and users."
+                    projectImg={bms}
+                    gitlink={"https://github.com/Neo-Cicada/Thesis-Barangay-Management-System"}
+                    />
+                <ProjectBox number="2" name='Bookish Boulevard' />
+                <ProjectBox number="3" name='Rapid Room' />
 
             </div>
         </Element>
