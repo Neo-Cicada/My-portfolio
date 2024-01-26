c5f1ed3bc11e290f51a9f0f75d8860455527e1ad
Neo <lordneobarnachea@gmail.com>
2024-01-26 20:37:43 +0800

    add resume

--- src/components/common/ResumeBtn.jsx
+++ src/components/common/ResumeBtn.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import download from '../../assets/icons/download.svg'
-
+import resumePDF from '../../assets/resume.pdf'
 
 const ResumeBtn = () => {
+    const handleButtonClick = () => {
+        // Open a new tab with the PDF URL
+        window.open(resumePDF, '_blank');
+    };
+
     return (
-        <div className='cursor-pointer h-full w-[153px] bg-black text-primary-white flex justify-center items-center gap-3 rounded-[4px]'>
-           <p className='text-lg font-semibold'>Resume</p>
-           <img src={download} alt="download icon" />
+        <div
+            className='cursor-pointer h-full w-[153px] bg-black text-primary-white flex justify-center items-center gap-3 rounded-[4px]'
+            onClick={handleButtonClick}
+        >
+            <p className='text-lg font-semibold'>Resume</p>
+            <img src={download} alt="download icon" />
         </div>
     )
 }
